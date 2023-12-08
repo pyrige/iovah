6fe879b71ba78163310c33c5b05814794586880d
Kevin Perry <perry@coffee-break.at>
2023-12-08 16:04:39 +0100

    chore: omit superfluous function

--- islands/Encoder.tsx
+++ islands/Encoder.tsx
@@ -5,20 +5,16 @@ import { encode } from "../util/Encoder.ts";
 export default function Counter() {
   const output = useSignal(" ");
 
-  function encodeText(input: string | null) {
-    if (input === null) return;
-
-    output.value = encode(input);
-  }
-
   return (
     <div class="flex flex-col gap-8 py-6 items-center justify-center">
       <div
         contenteditable
         class="border-0 border-b-4 border-zinc-200 focus:border-blue-400 outline-none bg-transparent text-3xl w-32 min-w-max inline-block capitalize"
-        onInput={(e) => {
-          const input = e.currentTarget.textContent;
-          encodeText(input);
+        onInput={(ev) => {
+          const input = ev.currentTarget.textContent;
+          if (input) {
+            output.value = encode(input);
+          }
         }}
       />
 
